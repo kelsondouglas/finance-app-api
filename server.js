85fb902c7aea67f50a69b61ae9ef2c51b9fc0f4b
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-02-16 20:39:20 -0300

    feat: add getUserByIdController and get route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import { CreateUserController } from "./src/controllers/create-user.js";
+import { GetUserByIdController } from "./src/controllers/get-user-by-id.js";
 
 const app = express();
 
@@ -10,6 +11,14 @@ app.post("/users", async (req, res) => {
   const createUserController = new CreateUserController();
 
   const { statusCode, body } = await createUserController.execute(req);
+
+  res.status(statusCode).json(body);
+});
+
+app.get("/users/:userId", async (req, res) => {
+  const getUserByIdController = new GetUserByIdController();
+
+  const { statusCode, body } = await getUserByIdController.execute(req);
 
   res.status(statusCode).json(body);
 });
