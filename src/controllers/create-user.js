0db45a5d8e4f1b28d768d2d23430e52678aadf8a
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-02-16 11:47:11 -0300

    refactor: add helper func to return requests

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,5 +1,6 @@
 import validator from "validator";
 import { CreateUserUseCase } from "../use-cases/create-user.js";
+import { badRequest, created, serverError } from "./helpers.js";
 
 export class CreateUserController {
   async execute(httpRequest) {
@@ -12,53 +13,33 @@ export class CreateUserController {
 
       for (const field of requiredFields) {
         if (!params[field] || !params[field].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Field ${field} is required`,
-            },
-          };
+          return badRequest({
+            message: `Field ${field} is required`,
+          });
         }
       }
 
       const passwordIsInvalid = params.password.length < 6;
       if (passwordIsInvalid) {
-        return {
-          statusCode: 400,
-          body: {
-            errorMessage: "Password must be at least 6 characters long",
-          },
-        };
+        return badRequest({
+          message: "Password must be at least 6 characters long",
+        });
       }
 
       //validar o email
       const isEmailValid = validator.isEmail(params.email);
       if (!isEmailValid) {
-        return {
-          statusCode: 400,
-          body: {
-            errorMessage: "Email is invalid",
-          },
-        };
+        return badRequest({ message: "Email is invalid." });
       }
 
       //chamar o use case
       const createUserUseCase = new CreateUserUseCase();
-
       const createdUser = await createUserUseCase.execute(params);
 
-      return {
-        statusCode: 201,
-        body: createdUser,
-      };
+      return created(createdUser);
     } catch (error) {
       console.error(error);
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: "Internal Server Error",
-        },
-      };
+      return serverError();
     }
   }
 }
