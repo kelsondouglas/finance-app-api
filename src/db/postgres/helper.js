9fb36da0ecd5417a6e89c56536b94be684885c21
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-02-16 08:15:00 -0300

    feat: add init migration to create users and transactions table

--- src/db/postgres/helper.js
+++ src/db/postgres/helper.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const pool = new Pool({
+export const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
