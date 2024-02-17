d21aad5aadf9bfcf0a4a29b8824020c8b1ac1a7d
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-02-17 11:45:53 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import { EmailAlreadyInUseError } from "../errors/user.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user.js";
+
+export class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    const postgresGetUserByEmailRepository =
+      new PostgresGetUserByEmailRepository();
+
+    const userWithProvidedEmail = postgresGetUserByEmailRepository.execute(
+      updateUserParams.email,
+    );
+
+    if (userWithProvidedEmail) {
+      throw new EmailAlreadyInUseError(updateUserParams.email);
+    }
+
+    const user = {
+      ...updateUserParams,
+    };
+
+    if (updateUserParams.password) {
+      const hashedPassword = await bcrypt.hash(updateUserParams.password, 10);
+
+      user.password = hashedPassword;
+    }
+
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+    const updatedUser = await postgresUpdateUserRepository.execute(
+      userId,
+      user,
+    );
+
+    return updatedUser;
+  }
+}
