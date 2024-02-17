42f737e4703f7ec53f5594b87d7fc1fbe3562703
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-02-17 10:34:24 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,26 @@
+import { postgresHelper } from "../../db/postgres/helper.js";
+
+export class PostgresUpdateUserRepository {
+  async execute(userId, updateUserParams) {
+    const updateField = [];
+    const updateValues = [];
+
+    Object.keys(updateUserParams).forEach((key) => {
+      updateField.push(`${key} = $${updateValues.length + 1}`);
+      updateValues.push(updateUserParams[key]);
+    });
+
+    updateValues.push(userId);
+
+    const updateQuery = `
+    UPDATE users
+    SET ${updateField.join(", ")}
+    WHERE id = $${updateValues.length}
+    RETURNING *
+    `;
+
+    const updatedUser = await postgresHelper.query(updateQuery, [updateValues]);
+
+    return updatedUser[0];
+  }
+}
