e188a2164ea7925dece7bec4072ae32faa12583a
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-03-07 13:57:12 -0300

    fix: ensure that transaction amount is number and had two digitals after decimals

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -9,7 +9,9 @@ export const invalidTypeResponse = () =>
     message: "The type must be EARNING, EXPENSE or INVESTMENT.",
   });
 export const checkIfAmountIsValid = (amount) => {
-  const isValid = validator.isCurrency(amount.toString(), {
+  if (typeof amount !== "number") return false;
+
+  const isValid = validator.isCurrency(amount.toFixed(2), {
     digits_after_decimal: [2],
     decimal_separator: ".",
     allow_negatives: false,
