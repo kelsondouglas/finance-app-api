4380f7a5f016718714f23ae46fdb58e29bc1a255
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-03-08 09:23:58 -0300

    feat: config SQL migrations

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -3,21 +3,13 @@ import { postgresHelper } from "../../../db/postgres/helper.js";
 export class PostgresGetUserBalanceRepository {
   async execute(userId) {
     const balance = await postgresHelper.query(
-      `SELECT
-        SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-        SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-        SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
-        (
-            SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) 
-            - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) 
-            - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
-        ) AS balance
-        FROM transactions
-        WHERE user_id = $1;
-        `,
+      `SELECT * from get_user_balance($1)`,
       [userId],
     );
 
-    return { userId, ...balance[0] };
+    return {
+      userId,
+      ...balance[0],
+    };
   }
 }
